2c30e42222d83b254f2980ce6b12456f26451cff
peddn <public@peddn.net>
2020-12-26 10:54:03 +0100

    development

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # admintools
 Some python scripts to help automate a nginx, gunicorn, wagtail, postgresql stack.
+
+TODO
+====
+
+installationsanleitungen
+
+    * first time setup
+    * nginx
+    * djnago, postgresql, nginx, gunicorn
+
+* basisabhängigkeiten sammeln
+
+* installation in unabhängige abschnitte unterteilen
+
+* diese abschnitte in tools umetzen
+
+    * first time setup
+    * update
+
+
+
+How to install multiple packages via textfile:
+xargs -a ./install.txt sudo apt-get install -y
--- admin.py
+++ admin.py
@@ -1,13 +1,23 @@
+from subprocess import run, CalledProcessError
 import click
 
 @click.command()
-@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Your name',
-              help='The person to greet.')
-def hello(count, name):
-    """Simple program that greets NAME for a total of COUNT times."""
-    for x in range(count):
-        click.echo('Hello %s!' % name)
+@click.option('-p', '--password', prompt=True, hide_input=True,
+              confirmation_prompt=False, required=True)
+
+def admin(password):
+    """Doc String"""
+
+    apt_get_update = None
+
+    try:
+        apt_get_update = run(['sudo', '-S', 'apt-get', 'update'], capture_output=True, text=True, input=password, check=True)
+    except CalledProcessError as error:
+        click.echo(error)
+
+    if apt_get_update is not None:
+        click.echo(apt_get_update.stdout)
+
 
 if __name__ == '__main__':
-    hello()
+    admin()
