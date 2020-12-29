48d2c8f8289785489046d97b1f52841eaa31eadb
peddn <public@peddn.net>
2020-12-29 17:11:49 +0100

    testing

--- db_setup.py
+++ db_setup.py
@@ -2,13 +2,17 @@ import psycopg2
 
 db_username = 'peddn'
 
+print('Executing db_setup script.')
+
 conn = psycopg2.connect('dbname=postgres')
 
 cur = conn.cursor()
 
 result  = cur.execute('ALTER ROLE ' + db_username + ' SET client_encoding TO ' + "'utf8'" + ';')
 
-print(result)
+print('RESULT: ' + result)
 
 cur.close()
 conn.close()
+
+print('Done.')
