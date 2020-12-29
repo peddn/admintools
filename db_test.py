35e468e6cc6824abc6cd5e517b4979f532ba31fe
peddn <public@peddn.net>
2020-12-29 16:10:57 +0100

    testing

---
+++ db_test.py
@@ -0,0 +1,14 @@
+import psycopg2
+
+db_name = 'peddn'
+db_username = 'peddn'
+
+conn = psycopg2.connect('dbname=' + db_name + ' user=' + db_username)
+
+cur = conn.cursor()
+
+cur.execute('ALTER ROLE ' + db_username + ' SET client_encoding TO ' + "'utf8'" + ';"')
+
+cur.close()
+conn.close()
+
