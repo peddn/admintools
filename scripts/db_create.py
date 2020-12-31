6babe2fedcd72a50aa8ce8346ebe3e051fe884c4
peddn <public@peddn.net>
2020-12-31 08:34:33 +0100

    restructuring

---
+++ scripts/db_create.py
@@ -0,0 +1,43 @@
+import sys
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+from psycopg2.errors import DuplicateDatabase, DuplicateObject
+
+project = sys.argv[1]
+db_password = sys.argv[2]
+
+print('Executing db_setup script.')
+
+# this script assumes that it runs as postgres unix user
+# connect to the 'postgres' standard database
+con = psycopg2.connect('dbname=postgres')
+
+
+con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+cur = con.cursor()
+
+try:
+    cur.execute('CREATE DATABASE ' + project + ';')
+except DuplicateDatabase as error:
+    print(error)
+
+try:
+    cur.execute('CREATE USER ' + project + " WITH PASSWORD '" + db_password + "';")
+except DuplicateObject as error:
+    print(error)
+else:
+    cur.execute('ALTER ROLE ' + project + ' SET client_encoding TO ' + "'utf8'" + ';')
+    cur.execute('ALTER ROLE ' + project + ' SET default_transaction_isolation TO ' + "'read committed'" + ';')
+    cur.execute('ALTER ROLE ' + project + ' SET timezone TO ' + "'UTC'" + ';')
+    cur.execute('GRANT ALL PRIVILEGES ON DATABASE ' + project + ' TO ' + project + ';')
+
+cur.execute('SELECT * FROM pg_catalog.pg_roles')
+for row in cur.fetchall():
+    if str(row).startswith(project):
+        print(str(row))
+
+cur.close()
+con.close()
+
+print('Done.')
---
+++ scripts/db_drop.py
@@ -0,0 +1,29 @@
+import sys
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+from psycopg2.errors import DuplicateDatabase, DuplicateObject
+
+project = sys.argv[1]
+db_password = sys.argv[2]
+
+print('Executing db_setup script.')
+
+# this script assumes that it runs as postgres unix user
+# connect to the 'postgres' standard database
+con = psycopg2.connect('dbname=postgres')
+
+
+con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+cur = con.cursor()
+
+
+cur.execute('DROP DATABASE ' + project + ';')
+
+cur.execute('DROP USER ' + project + ';')
+
+
+cur.close()
+con.close()
+
+print('Done.')
